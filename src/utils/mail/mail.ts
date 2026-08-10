80745cbf7b3d6ca45c71884fb6426b08951d593f
Decayy25 <mochrizqihermawan615@gmail.com>
2026-08-10 12:45:52 +0700

    fix: error log vercel

--- src/utils/mail/mail.ts
+++ src/utils/mail/mail.ts
@@ -1,7 +1,6 @@
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
-import { fileURLToPath } from "url";
 import {
   EMAIL_SMTP_HOST,
   EMAIL_SMTP_PASS,
@@ -41,8 +40,6 @@ export const renderMailHTML = async (
   template: string,
   data: any,
 ): Promise<string> => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
   const content = await ejs.renderFile(
     path.join(__dirname, `templates/${template}`),
     data,
